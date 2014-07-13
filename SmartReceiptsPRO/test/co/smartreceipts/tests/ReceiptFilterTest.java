69359e2be3647a44c2b26f4175c731494aac5dc8
Akhyar Amarullah <akhyrul@gmail.com>
2014-07-13 20:08:01 +0700

    Added unit tests for all receipt filters

--- SmartReceiptsPRO/test/co/smartreceipts/tests/ReceiptFilterTest.java
+++ SmartReceiptsPRO/test/co/smartreceipts/tests/ReceiptFilterTest.java
@@ -1,10 +1,14 @@
 package co.smartreceipts.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.json.JSONException;
 import org.junit.After;
@@ -18,12 +22,20 @@ import org.robolectric.annotation.Config;
 import co.smartreceipts.android.SmartReceiptsApplication;
 import co.smartreceipts.android.filters.Filter;
 import co.smartreceipts.android.filters.FilterFactory;
+import co.smartreceipts.android.filters.ReceiptAndFilter;
 import co.smartreceipts.android.filters.ReceiptCategoryFilter;
+import co.smartreceipts.android.filters.ReceiptIsExpensableFilter;
+import co.smartreceipts.android.filters.ReceiptMaximumPriceFilter;
+import co.smartreceipts.android.filters.ReceiptMinimumPriceFilter;
+import co.smartreceipts.android.filters.ReceiptNotFilter;
+import co.smartreceipts.android.filters.ReceiptOnOrAfterDayFilter;
+import co.smartreceipts.android.filters.ReceiptOnOrBeforeDayFilter;
 import co.smartreceipts.android.filters.ReceiptOrFilter;
+import co.smartreceipts.android.filters.ReceiptSelectedFilter;
 import co.smartreceipts.android.model.ReceiptRow;
 import co.smartreceipts.tests.utils.ReceiptUtils.Constants;
 
-@Config(emulateSdk = 18) 
+@Config(emulateSdk = 18, manifest = "../SmartReceiptsPRO/AndroidManifest.xml") 
 @RunWith(RobolectricTestRunner.class)
 public class ReceiptFilterTest {
 	
@@ -81,6 +93,106 @@ public class ReceiptFilterTest {
 	}
 	
 	@Test
+	public void receiptIsExpensableFilterTest() throws JSONException {
+		final ReceiptRow receipt1 = getGenericReceiptRowBuilder().setIsExpenseable(true).build();
+		final ReceiptRow receipt2 = getGenericReceiptRowBuilder().setIsExpenseable(false).build();
+		final ReceiptIsExpensableFilter filter = new ReceiptIsExpensableFilter();
+		
+		assertTrue(filter.accept(receipt1));
+		assertFalse(filter.accept(receipt2));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptIsSelectedFilterTest() throws JSONException {
+		final ReceiptRow receipt1 = getGenericReceiptRowBuilder().setIsSelected(true).build();
+		final ReceiptRow receipt2 = getGenericReceiptRowBuilder().setIsSelected(false).build();
+		final ReceiptSelectedFilter filter = new ReceiptSelectedFilter();
+		
+		assertTrue(filter.accept(receipt1));
+		assertFalse(filter.accept(receipt2));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptMinimumPriceFilterTest() throws JSONException {
+		final String priceNormal = "100.0";
+		final String priceHigh = "150.0";
+		final String priceLow = "50.0";
+		
+		final ReceiptRow receiptNormal = getGenericReceiptRowBuilder().setPrice(priceNormal).build();
+		final ReceiptRow receiptHigh = getGenericReceiptRowBuilder().setPrice(priceHigh).build();
+		final ReceiptRow receiptLow = getGenericReceiptRowBuilder().setPrice(priceLow).build();
+		
+		final ReceiptMinimumPriceFilter filter = new ReceiptMinimumPriceFilter(
+				Float.parseFloat(priceNormal), 
+				Constants.CURRENCY_CODE);
+		
+		assertTrue(filter.accept(receiptNormal));
+		assertTrue(filter.accept(receiptHigh));
+		assertFalse(filter.accept(receiptLow));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptMaximumPriceFilterTest() throws JSONException {
+		final String priceNormal = "100.0";
+		final String priceHigh = "150.0";
+		final String priceLow = "50.0";
+		
+		final ReceiptRow receiptNormal = getGenericReceiptRowBuilder().setPrice(priceNormal).build();
+		final ReceiptRow receiptHigh = getGenericReceiptRowBuilder().setPrice(priceHigh).build();
+		final ReceiptRow receiptLow = getGenericReceiptRowBuilder().setPrice(priceLow).build();
+		
+		final ReceiptMaximumPriceFilter filter = new ReceiptMaximumPriceFilter(
+				Float.parseFloat(priceNormal), 
+				Constants.CURRENCY_CODE);
+		
+		assertTrue(filter.accept(receiptNormal));
+		assertFalse(filter.accept(receiptHigh));
+		assertTrue(filter.accept(receiptLow));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptOnOrAfterDayFilterTest() throws JSONException {
+		final TimeZone tz = TimeZone.getDefault();
+		final long nowMillis = new java.util.Date().getTime();
+		final Date now = new Date(nowMillis);
+		final Date future = new Date(nowMillis + 10000);
+		final Date past = new Date(nowMillis - 10000);
+
+		final ReceiptRow receiptNow = getGenericReceiptRowBuilder().setDate(now).build();
+		final ReceiptRow receiptFuture = getGenericReceiptRowBuilder().setDate(future).build();
+		final ReceiptRow receiptPast = getGenericReceiptRowBuilder().setDate(past).build();
+		final ReceiptOnOrAfterDayFilter filter = new ReceiptOnOrAfterDayFilter(now, tz);
+		
+		assertTrue(filter.accept(receiptNow));
+		assertTrue(filter.accept(receiptFuture));
+		assertFalse(filter.accept(receiptPast));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptOnOrBeforeDayFilterTest() throws JSONException {
+		final TimeZone tz = TimeZone.getDefault();
+		final long nowMillis = new java.util.Date().getTime();
+		final Date now = new Date(nowMillis);
+		final Date future = new Date(nowMillis + 10000);
+		final Date past = new Date(nowMillis - 10000);
+
+		final ReceiptRow receiptNow = getGenericReceiptRowBuilder().setDate(now).build();
+		final ReceiptRow receiptFuture = getGenericReceiptRowBuilder().setDate(future).build();
+		final ReceiptRow receiptPast = getGenericReceiptRowBuilder().setDate(past).build();
+		final ReceiptOnOrBeforeDayFilter filter = new ReceiptOnOrBeforeDayFilter(now, tz);
+		
+		assertTrue(filter.accept(receiptNow));
+		assertFalse(filter.accept(receiptFuture));
+		assertTrue(filter.accept(receiptPast));
+		assertEquals(filter, FilterFactory.getReceiptFilter(filter.getJsonRepresentation()));
+	}
+	
+	@Test
 	public void receiptOrFilterConstructorTest() throws JSONException {
 		final ReceiptCategoryFilter filter1 = new ReceiptCategoryFilter(Constants.CATEGORY);
 		final ReceiptCategoryFilter filter2 = new ReceiptCategoryFilter("cat2");
@@ -109,5 +221,34 @@ public class ReceiptFilterTest {
 		assertTrue(orFilter.accept(receipt2));
 		assertFalse(orFilter.accept(receipt3));
 		assertEquals(orFilter, FilterFactory.getReceiptFilter(orFilter.getJsonRepresentation())); // Confirm we can properly recreate
+	}
+	
+	@Test
+	public void receiptAndFilterTest() throws JSONException {
+		final ReceiptRow receipt = getGenericReceiptRowBuilder().build();
+		
+		final ReceiptIsExpensableFilter trueFilter1 = new ReceiptIsExpensableFilter();
+		final ReceiptCategoryFilter trueFilter2 = new ReceiptCategoryFilter(Constants.CATEGORY);
+		final ReceiptCategoryFilter falseFilter = new ReceiptCategoryFilter("BAD Category");
+		
+		final ReceiptAndFilter andFilterGood = new ReceiptAndFilter();
+		final ReceiptAndFilter andFilterBad = new ReceiptAndFilter();
+		andFilterGood.and(trueFilter1).and(trueFilter2);
+		andFilterBad.and(trueFilter1).and(trueFilter2).and(falseFilter);
+		
+		assertTrue(andFilterGood.accept(receipt));
+		assertFalse(andFilterBad.accept(receipt));
+		assertEquals(andFilterGood, FilterFactory.getReceiptFilter(andFilterGood.getJsonRepresentation()));
+		assertEquals(andFilterBad, FilterFactory.getReceiptFilter(andFilterBad.getJsonRepresentation()));
+	}
+	
+	@Test
+	public void receiptNotFilterTest() throws JSONException {
+		final ReceiptRow receipt = getGenericReceiptRowBuilder().build();
+		final ReceiptCategoryFilter filter = new ReceiptCategoryFilter(Constants.CATEGORY);
+		final ReceiptNotFilter notFilter = new ReceiptNotFilter(filter);
+		
+		assertFalse(notFilter.accept(receipt));
+		assertEquals(notFilter, FilterFactory.getReceiptFilter(notFilter.getJsonRepresentation()));
 	}
 }

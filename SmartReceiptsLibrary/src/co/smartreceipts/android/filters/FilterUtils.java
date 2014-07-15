caf17de82855ab20f9b1965b1e9bcae3d1449c87
Akhyar Amarullah <akhyrul@gmail.com>
2014-07-15 11:26:25 +0700

    Added chronological order to timezone test

--- SmartReceiptsLibrary/src/co/smartreceipts/android/filters/FilterUtils.java
+++ SmartReceiptsLibrary/src/co/smartreceipts/android/filters/FilterUtils.java
@@ -1,28 +1,21 @@
 package co.smartreceipts.android.filters;
 
 import java.sql.Date;
-import java.util.Calendar;
 import java.util.TimeZone;
 
 public class FilterUtils {
 
 	public static boolean isOnOrBefore(Date dt1, TimeZone tz1, Date dt2, TimeZone tz2) {
-		Calendar cal1 = Calendar.getInstance(tz1);
-		cal1.setTime(dt1);
+		long m1 = dt1.getTime();
+		long m2 = dt2.getTime();
 
-		Calendar cal2 = Calendar.getInstance(tz2);
-		cal2.setTime(dt2);
-
-		return cal1.getTimeInMillis() <= cal2.getTimeInMillis();
+		return m1 - tz1.getRawOffset() <= m2 - tz2.getRawOffset();
 	}
 
 	public static boolean isOnOrAfter(Date dt1, TimeZone tz1, Date dt2, TimeZone tz2) {
-		Calendar cal1 = Calendar.getInstance(tz1);
-		cal1.setTime(dt1);
+		long m1 = dt1.getTime();
+		long m2 = dt2.getTime();
 
-		Calendar cal2 = Calendar.getInstance(tz2);
-		cal2.setTime(dt2);
-
-		return cal1.getTimeInMillis() >= cal2.getTimeInMillis();
+		return m1 - tz1.getRawOffset() >= m2 - tz2.getRawOffset();
 	}
 }

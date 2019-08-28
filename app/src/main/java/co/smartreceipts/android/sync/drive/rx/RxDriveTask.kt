22bd3b3d6e24c89416832b739b00ab436a0b4279
ministeve412 <ministeve412@gmail.com>
2019-08-28 10:43:11 -0700

    converting to extension functions

--- app/src/main/java/co/smartreceipts/android/sync/drive/rx/RxDriveTask.kt
+++ app/src/main/java/co/smartreceipts/android/sync/drive/rx/RxDriveTask.kt
@@ -8,97 +8,90 @@ import io.reactivex.Observable
 import io.reactivex.Single
 
 /**
- * Provides us with some Rx extensions around the [Task] object that is used for Google Drive
- * integrations
- */
-object RxDriveTask {
+ * Converts a [Task] into a [Single]. All operations will be run on our
+ * [SyncSchedulers]
+ *
+ * @param task the [Task] to convert
+ * @param <TResult> the result type of this [Task]
+ * @return a [Single], which will emit onSuccess/onError based on the results of the task
+</TResult> */
+fun <TResult> Task<TResult>.toSingle(): Single<TResult> {
+    return Single.create<TResult> { emitter ->
+        addOnSuccessListener { result ->
+            if (!emitter.isDisposed) {
+                emitter.onSuccess(result)
+            }
+        }
+        addOnFailureListener { throwable ->
+            if (!emitter.isDisposed) {
+                emitter.onError(throwable)
+            }
+        }
+        addOnCanceledListener {
+            if (!emitter.isDisposed) {
+                emitter.onError(InterruptedException("This drive task was cancelled"))
+            }
+        }
+    }.subscribeOn(SyncSchedulers.io())
+}
 
-    /**
-     * Converts a [Task] into a [Single]. All operations will be run on our
-     * [SyncSchedulers]
-     *
-     * @param task the [Task] to convert
-     * @param <TResult> the result type of this [Task]
-     * @return a [Single], which will emit onSuccess/onError based on the results of the task
-    </TResult> */
-    fun <TResult> toSingle(task: Task<TResult>): Single<TResult> {
-        return Single.create<TResult> { emitter ->
-            task.addOnSuccessListener { result ->
-                if (!emitter.isDisposed) {
-                    emitter.onSuccess(result)
-                }
+/**
+ * Converts a [Task] into an [Observable]. All operations will be run on our
+ * [SyncSchedulers]
+ *
+ * @param task the [Task] to convert
+ * @param <TResult> the result type of this [Task]
+ * @return a [Observable], which will emit onNext/onError based on the results of the task
+</TResult> */
+fun <TResult> Task<TResult>.toObservable(): Observable<TResult> {
+    return Observable.create<TResult> { emitter ->
+        addOnSuccessListener { result ->
+            if (!emitter.isDisposed) {
+                emitter.onNext(result)
             }
-            task.addOnFailureListener { throwable ->
-                if (!emitter.isDisposed) {
-                    emitter.onError(throwable)
-                }
+        }
+        addOnCompleteListener { originalTask ->
+            if (!emitter.isDisposed) {
+                emitter.onComplete()
             }
-            task.addOnCanceledListener {
-                if (!emitter.isDisposed) {
-                    emitter.onError(InterruptedException("This drive task was cancelled"))
-                }
+        }
+        addOnFailureListener { throwable ->
+            if (!emitter.isDisposed) {
+                emitter.onError(throwable)
             }
-        }.subscribeOn(SyncSchedulers.io())
-    }
+        }
+        addOnCanceledListener {
+            if (!emitter.isDisposed) {
+                emitter.onError(InterruptedException("This drive task was cancelled"))
+            }
+        }
+    }.subscribeOn(SyncSchedulers.io())
+}
 
-    /**
-     * Converts a [Task] into an [Observable]. All operations will be run on our
-     * [SyncSchedulers]
-     *
-     * @param task the [Task] to convert
-     * @param <TResult> the result type of this [Task]
-     * @return a [Observable], which will emit onNext/onError based on the results of the task
-    </TResult> */
-    fun <TResult> toObservable(task: Task<TResult>): Observable<TResult> {
-        return Observable.create<TResult> { emitter ->
-            task.addOnSuccessListener { result ->
-                if (!emitter.isDisposed) {
-                    emitter.onNext(result)
-                }
+/**
+ * Converts a [Task] into an [Completable]. All operations will be run on our
+ * [SyncSchedulers]
+ *
+ * @param task the [Task] to convert
+ * @param <TResult> the result type of this [Task]
+ * @return a [Completable], which will emit onComplete/onError based on the results of the task
+</TResult> */
+fun <TResult> Task<TResult>.toCompletable(): Completable {
+    return Completable.create { emitter ->
+        addOnCompleteListener { originalTask ->
+            if (!emitter.isDisposed) {
+                emitter.onComplete()
             }
-            task.addOnCompleteListener { originalTask ->
-                if (!emitter.isDisposed) {
-                    emitter.onComplete()
-                }
+        }
+        addOnFailureListener { throwable ->
+            if (!emitter.isDisposed) {
+                emitter.onError(throwable)
             }
-            task.addOnFailureListener { throwable ->
-                if (!emitter.isDisposed) {
-                    emitter.onError(throwable)
-                }
+        }
+        addOnCanceledListener {
+            if (!emitter.isDisposed) {
+                emitter.onError(InterruptedException("This drive task was cancelled"))
             }
-            task.addOnCanceledListener {
-                if (!emitter.isDisposed) {
-                    emitter.onError(InterruptedException("This drive task was cancelled"))
-                }
-            }
-        }.subscribeOn(SyncSchedulers.io())
-    }
-
-    /**
-     * Converts a [Task] into an [Completable]. All operations will be run on our
-     * [SyncSchedulers]
-     *
-     * @param task the [Task] to convert
-     * @param <TResult> the result type of this [Task]
-     * @return a [Completable], which will emit onComplete/onError based on the results of the task
-    </TResult> */
-    fun <TResult> toCompletable(task: Task<TResult>): Completable {
-        return Completable.create { emitter ->
-            task.addOnCompleteListener { originalTask ->
-                if (!emitter.isDisposed) {
-                    emitter.onComplete()
-                }
-            }
-            task.addOnFailureListener { throwable ->
-                if (!emitter.isDisposed) {
-                    emitter.onError(throwable)
-                }
-            }
-            task.addOnCanceledListener {
-                if (!emitter.isDisposed) {
-                    emitter.onError(InterruptedException("This drive task was cancelled"))
-                }
-            }
-        }.subscribeOn(SyncSchedulers.io())
-    }
+        }
+    }.subscribeOn(SyncSchedulers.io())
 }

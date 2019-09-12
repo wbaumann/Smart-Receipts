e5aa2f2d607bba544ea0665bf7dc4cd6c1df13b4
Julia Soboleva <julia.soboleva26@gmail.com>
2019-09-12 17:09:26 +0300

    Refactored `ReceiptsListFragment`, removed ocr scanning from `ActivityFileResultImporter`, fixed bug with ocr + crop

--- app/src/main/java/co/smartreceipts/android/imports/AttachmentSendFileImporter.kt
+++ app/src/main/java/co/smartreceipts/android/imports/AttachmentSendFileImporter.kt
@@ -3,34 +3,31 @@ package co.smartreceipts.android.imports
 import android.content.Context
 import co.smartreceipts.android.analytics.Analytics
 import co.smartreceipts.android.analytics.events.ErrorEvent
+import co.smartreceipts.android.di.scopes.ApplicationScope
 import co.smartreceipts.android.imports.intents.model.FileType
 import co.smartreceipts.android.imports.intents.model.IntentImportResult
 import co.smartreceipts.android.model.Receipt
 import co.smartreceipts.android.model.Trip
 import co.smartreceipts.android.model.factory.ReceiptBuilderFactory
-import co.smartreceipts.android.persistence.PersistenceManager
 import co.smartreceipts.android.persistence.database.controllers.impl.ReceiptTableController
 import co.smartreceipts.android.persistence.database.operations.DatabaseOperationMetadata
 import co.smartreceipts.android.settings.UserPreferenceManager
 import io.reactivex.Single
 import wb.android.storage.StorageManager
 import java.io.File
+import javax.inject.Inject
 
-class AttachmentSendFileImporter(
+@ApplicationScope
+class AttachmentSendFileImporter @Inject constructor(
     private val context: Context,
-    private val trip: Trip,
     private val storageManager: StorageManager,
     private val preferences: UserPreferenceManager,
     private val receiptTableController: ReceiptTableController,
     private val analytics: Analytics
 ) {
 
-    constructor(
-        context: Context, trip: Trip, persistenceManager: PersistenceManager,
-        receiptTableController: ReceiptTableController, analytics: Analytics
-    ) : this(context, trip, persistenceManager.storageManager, persistenceManager.preferenceManager, receiptTableController, analytics)
 
-    fun importAttachment(intentImportResult: IntentImportResult, receipt: Receipt): Single<File> {
+    fun importAttachment(trip: Trip, receipt: Receipt, intentImportResult: IntentImportResult): Single<File> {
 
         val importProcessor: FileImportProcessor = when {
             intentImportResult.fileType == FileType.Image -> ImageImportProcessor(trip, storageManager, preferences, context)

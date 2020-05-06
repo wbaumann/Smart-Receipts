00bf8660c3600181cbcf97a338ef50dda9fb4d46
ministeve412 <ministeve412@gmail.com>
2020-05-06 14:05:08 -0700

    updating dependencies

--- app/src/androidTest/java/co/smartreceipts/android/test/espresso/BaseEspressoTests.kt
+++ app/src/androidTest/java/co/smartreceipts/android/test/espresso/BaseEspressoTests.kt
@@ -4,13 +4,12 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
-import android.view.View
 import co.smartreceipts.android.R
 import co.smartreceipts.android.activities.SmartReceiptsActivity
-import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith

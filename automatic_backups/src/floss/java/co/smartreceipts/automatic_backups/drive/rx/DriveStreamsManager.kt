0a41d24e3cde71fb7111d8fb0c82379066a79bca
ministeve412 <ministeve412@gmail.com>
2020-07-26 09:21:30 -0700

    updating import line

--- automatic_backups/src/floss/java/co/smartreceipts/automatic_backups/drive/rx/DriveStreamsManager.kt
+++ automatic_backups/src/floss/java/co/smartreceipts/automatic_backups/drive/rx/DriveStreamsManager.kt
@@ -8,6 +8,7 @@ import co.smartreceipts.core.sync.model.RemoteBackupMetadata
 import co.smartreceipts.core.sync.model.SyncState
 import co.smartreceipts.core.sync.model.impl.Identifier
 import com.hadisatrio.optional.Optional
+import com.google.api.services.drive.model.File
 import io.reactivex.Single
 import io.reactivex.subjects.Subject
 
@@ -27,7 +28,7 @@ class DriveStreamsManager(context: Context, driveServiceHelper: DriveServiceHelp
 
     fun updateDriveFile(currentSyncState: SyncState, file: java.io.File): Single<SyncState?> = Single.never()
 
-    fun renameBackup(identifier: Identifier, newFileName: String): Single<com.google.api.services.drive.model.File> = Single.never()
+    fun renameBackup(identifier: Identifier, newFileName: String): Single<File> = Single.never()
 
     fun deleteDriveFile(currentSyncState: SyncState, isFullDelete: Boolean): Single<SyncState?> = Single.never()
 

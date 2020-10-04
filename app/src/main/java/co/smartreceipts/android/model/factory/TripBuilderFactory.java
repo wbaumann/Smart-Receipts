6f470973354d2ddcddb81b4f00d1818aa1ab5cf1
ministeve412 <ministeve412@gmail.com>
2020-10-04 14:02:22 -0700

    removing unused imports

--- app/src/main/java/co/smartreceipts/android/model/factory/TripBuilderFactory.java
+++ app/src/main/java/co/smartreceipts/android/model/factory/TripBuilderFactory.java
@@ -11,8 +11,6 @@ import org.joda.money.CurrencyUnit;
 
 import java.io.File;
 import java.sql.Date;
-import java.util.Currency;
-import java.util.Locale;
 import java.util.TimeZone;
 import java.util.UUID;
 
